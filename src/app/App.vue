f9159c917f02f36756ab05497fbd5f20795fb61f
diegofertr <diegonano09@gmail.com>
2018-01-04 13:21:41 -0400

    Read from api -> ready

---
+++ src/app/App.vue
@@ -0,0 +1,13 @@
+<template lang="html">
+  <div>
+    <h1> Vue2 Funcionando</h1>
+    <div class="container">
+      <router-view></router-view>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+}
+</script>
